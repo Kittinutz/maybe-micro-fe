7c5081107263e56d0aaea0596b8b2fed9940372c
Chankaseam.t <kzxp@msn.com>
2018-12-19 21:50:09 +0700

    Remove nuxtjs.

--- backend/nodejs/routes/lazada/index.js
+++ backend/nodejs/routes/lazada/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const fs = require('fs')
 const router = express.Router()
+const lazadaScrapper = require('../../web-scrapper/lazada')
 
 router.get('/', function (req, res) {
   res.status(200).json({ message: 'main' })
@@ -12,5 +14,17 @@ router.get('/hot', function (req, res) {
 router.get('/recommended', function (req, res) {
   res.status(200).json({ message: 'recommended' })
 })
+
+router.get('/generate', async function (req, res) {
+  await lazadaScrapper.scrapLazada()
+  const data = JSON.parse(fs.readFileSync('data/lazada-flash-sale.json'))
+  res.status(200).json(data)
+})
+
+router.get('/data', async function (req, res) {
+  const data = JSON.parse(fs.readFileSync('data/lazada-flash-sale.json'))
+  res.status(200).json(data)
+})
+
 
 module.exports = router
